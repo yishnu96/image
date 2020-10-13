003534826656602d141d7eeddf09bc7d57ec9119
Yishnu <yishnupramanik96@gmail.com>
2020-10-13 11:29:24 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,33 @@
-import { Component } from '@angular/core';
-import mergeImages from 'merge-images';
-import ImageEditor from 'tui-image-editor';
+import { Component, OnInit } from '@angular/core';
+import  ImageEditor from 'tui-image-editor';
 import FileSaver from 'file-saver';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   image = '../assets/img/questionpaper.png';
 
-  currect = '../assets/img/currectt.png';
-  wrong = '../assets/img/wrongg.png';
+  constructor() {
+  }
 
-  merge (number : number){
-    this.image = '../assets/img/questionpaper.png';
-    // console.log(this.image);
-    switch(number){
-      case 0: {
-        // mergeImages([this.image, this.wrong ])
-        // .then(b64 => this.image = b64);
-        // break;
-        mergeImages([
-          { src: this.image, x : 0, y : 0 },
-          { src: this.wrong, x : 600, y: 75 }
-        ])
-        .then(b64 => this.image = b64);
-        break;
+  public ngOnInit() {
+    const instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
+      cssMaxWidth: 700,
+      cssMaxHeight: 500,
+      selectionStyle: {
+          cornerSize: 20,
+          rotatingPointOffset: 70
       }
-
-      case 1: {
-        mergeImages([
-          { src: this.image, x : 0, y : 0 },
-          { src: this.currect, x : 600, y: 75 }
-        ])
-        .then(b64 => this.image = b64);
-        break;
-      }
-    }
+  });
   }
 
 
+// Load image
+// instance.loadImageFromURL('img/sampleImage.jpg', 'My sample image')
+
 }
+
+
